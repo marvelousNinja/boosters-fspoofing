69b3523fe4190ad6bce5d4109a7391f3dc0987fa
Alexander Sologub <alexsologoub@gmail.com>
2018-08-09 18:20:52 +0300

    Replace unzip with tar

--- download.sh
+++ download.sh
@@ -5,9 +5,9 @@ cd data
 wget "https://$STORAGE_CREDS@u184584.your-storagebox.de/test.tar.gz"
 wget "https://$STORAGE_CREDS@u184584.your-storagebox.de/IDRND_FASDB_val.tar.gz"
 wget "https://$STORAGE_CREDS@u184584.your-storagebox.de/IDRND_FASDB_train.tar.gz"
-unzip test.tar.gz
-unzip IDRND_FASDB_val.tar.gz
-unzip IDRND_FASDB_train.tar.gz
+tar -xvzf test.tar.gz
+tar -xvzf IDRND_FASDB_val.tar.gz
+tar -xvzf IDRND_FASDB_train.tar.gz
 rm test.tar.gz
 rm IDRND_FASDB_val.tar.gz
 rm IDRND_FASDB_train.tar.gz
